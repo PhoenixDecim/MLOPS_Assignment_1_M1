0157b3354ab502125decb501369326e57dda6b61
PhoenixDecim <adarshsivanandan11@gmail.com>
2025-01-11 02:25:24 +0530

    added standard scaler to preprocess routine and some minor additions

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,27 @@
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+
 def preprocess_data(data):
     # drop the missing values
-    data = data.dropna()
+    print(
+        "The dataset has {} rows and {} columns.".format(data.shape[0],
+                                                         data.shape[1])
+    )
+    null_count = data.isnull().sum()
+    total_nulls = null_count.sum()
+    if total_nulls == 0:
+        print("No null values found in the dataset.")
+    elif total_nulls > 0 and total_nulls < len(data):
+        print(f"{total_nulls} null values found, removing them.")
+        data = data.dropna()
+    else:
+        raise ValueError("All values are null in the dataset.")
     # select the features as X and target as y
     X = data.drop(columns=["variety"])
     y = data["variety"]
-    return X, y
+    scaler = StandardScaler()
+    print("Applying standard scaler to the input values.\n")
+    X_scaled = scaler.fit_transform(X)
+    X_scaled = pd.DataFrame(X_scaled, columns=X.columns)
+    return X_scaled, y
