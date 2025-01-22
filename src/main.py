c33cb11efd60476d40d7b4af011d6156b2527e20
PhoenixDecim <adarshsivanandan11@gmail.com>
2025-01-22 23:47:52 +0530

    added random forest model and model selection through arguments

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 from sklearn.model_selection import train_test_split
 from utils import save_model, load_model, load_dataset
 from train import train_model
@@ -6,7 +7,21 @@ from preprocess import preprocess_data
 from predict import predict
 import pandas as pd
 
-path = "models/logistic_regression_model.joblib"
+parser = argparse.ArgumentParser(description="Train a ML model")
+parser.add_argument(
+    "--model",
+    type=str,
+    required=True,
+    help="Available models: 'logistic_regression' or 'random_forest'",
+)
+args = parser.parse_args()
+model_name = args.model
+if model_name not in ["logistic_regression", "random_forest"]:
+    raise ValueError(
+        f"Unsupported model name: {model_name}. Choose 'logistic_regression'"
+        " or 'random_forest'"
+    )
+path = f"models/{model_name}_model.joblib"
 print("\n1. Loading Iris dataset\n")
 iris_df = load_dataset("data/iris.csv")
 print(iris_df.head(5))
@@ -22,7 +37,7 @@ print(f"Size of X_test: {X_test.shape}")
 print(f"Size of y_train: {y_train.shape}")
 print(f"Size of y_test: {y_test.shape}")
 print("\n4. Training the Logistic Regression model")
-model = train_model(X_train, y_train)
+model = train_model(X_train, y_train, model_name)
 print("\n5. Saving the model")
 save_status = save_model(model, path)
 print("\n6. Evaluating the model\n")
@@ -35,4 +50,10 @@ predictions = predict(loaded_model, X_test)
 results_df = pd.DataFrame(X_test, columns=X.columns)
 results_df["True Variety"] = y_test
 results_df["Predicted Variety"] = predictions
+# number of correct predictions
+correct_predictions = (
+    results_df["True Variety"] == results_df["Predicted Variety"]
+).sum()
+# Print the results
 print(results_df)
+print(f"Number of correct predictions: {correct_predictions}/{len(y_test)}")
