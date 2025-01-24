848de988691c189a9c92a4706ff1ddc9e47c1aa2
PhoenixDecim <adarshsivanandan11@gmail.com>
2025-01-25 00:22:26 +0530

    Added arguments for model parameters

--- src/train.py
+++ src/train.py
@@ -2,10 +2,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 
-def train_model(X_train, y_train, model_name):
+def train_model(X_train, y_train, model_name, **model_params):
     if model_name == "logistic_regression":
-        model = LogisticRegression(max_iter=200)
+        max_iter = model_params["max_iter"]
+        model = LogisticRegression(max_iter=max_iter)
     elif model_name == "random_forest":
-        model = RandomForestClassifier(n_estimators=100, random_state=42)
+        n_estimators = model_params["n_estimators"]
+        random_state = model_params["random_state"]
+        model = RandomForestClassifier(
+            n_estimators=n_estimators, random_state=random_state
+        )
     model.fit(X_train, y_train)
     return model
